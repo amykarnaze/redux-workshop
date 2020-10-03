ebdbc0cdec3ddc401d320c3d617c4cbc8be7b7b5
Amy Karnaze <akarnaze@gmail.com>
2020-10-03 16:37:16 -0600

    Add is complete reducer

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -6,6 +6,13 @@ export const todos = (state = [], action) => {
         todo: action.todo,
         completed: false
       }];
+    case 'IS_COMPLETE':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return { id: todo.id, todo: todo.text, completed: !todo.completed }
+        }
+        return todo;
+      });
     default:
       return state;
   }
