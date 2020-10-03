6ab93d4bdf9f1aa10abe609391de8bac7fb7afcc
Amy Karnaze <akarnaze@gmail.com>
2020-10-03 16:39:11 -0600

    Add todDoList to display todos

--- src/containers/ToDoList.js
+++ src/containers/ToDoList.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Todo from '../components/ToDo';
 import { connect } from 'react-redux';
+import { isComplete } from '../actions';
 
-const ToDoList = ({todos}) => {
-  console.log(todos);
+
+const ToDoList = ({ todos, isComplete }) => {
+  const displayTodos = todos.map(todo => {
+    return (
+      <Todo
+        {...todo}
+        key={todo.id}
+        id={todo.id}
+        isComplete={isComplete}
+      />
+    )
+  })
 
   return (
     <ul>
+      {displayTodos}
     </ul>
   )
 }
@@ -14,4 +27,4 @@ const mapStateToProps = state => ({
   todos: state.todos
 });
 
-export default connect(mapStateToProps)(ToDoList);
+export default connect(mapStateToProps, {isComplete})(ToDoList);
